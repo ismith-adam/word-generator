c18cc75c9e90b69c5219713fe8cd3d84f61f336d
Adam Smith <ismith.adam@gmail.com>
2020-07-31 13:34:25 -0400

    on click get word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,24 @@ class WordsController < ApplicationController
 
   def index
   end
+
+  def new
+    path = "#{Rails.public_path}/words.txt"
+    count = `wc -l "#{path}"`.strip.split(' ')[0].to_i
+    random_word_number = rand(1..count)
+    file = File.open(path, 'r')
+    line = ''
+    line_number = 0
+
+    while line = file.gets do
+      line_number += 1
+      if (random_word_number == line_number)
+        @word = line
+        break
+      end
+    end
+    file.close
+    
+    render plain: @word
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,13 +11,13 @@ require("channels")
 import "bootstrap"
 import "../stylesheets/application"
 
-document.addEventListener("turbolinks:load", () => {
-})
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
 const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(function(){
+  $("#getWord").click(function(event){
+    $.get("/words/new", function(word){
+      $("#showWord").text(word)
+    })
+  });
+});
