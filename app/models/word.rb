5a1fd9433c69cd56e7155782d49655baab78dbd5
Adam Smith <ismith.adam@gmail.com>
2020-07-31 16:43:40 -0400

    all passing of word number to get

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,15 +2,19 @@ class Word < ApplicationRecord
   WORDS_LIST_PATH = "#{Rails.public_path}/words.txt"
   
   def self.getFromFile(word_number=nil)
-    word_count = `wc -l "#{WORDS_LIST_PATH}"`.strip.split(' ')[0].to_i
-    random_word_number = rand(1..word_count)
+    if !word_number
+      word_count = `wc -l "#{WORDS_LIST_PATH}"`.strip.split(' ')[0].to_i
+      word_number = rand(1..word_count)
+    end
+
     file = File.open(WORDS_LIST_PATH, 'r')
     line = ''
     line_number = 0
 
+
     while line = file.gets do
       line_number += 1
-      if (random_word_number == line_number)
+      if (word_number == line_number)
         word = line
         break
       end
